76c76c4b29879ef8a8f73629df558d74829545b4
lukasz1812 <65965181+lukasz1812@users.noreply.github.com>
2020-11-12 23:21:30 +0100

    Update MolDyn.f90

--- MolDyn.f90
+++ MolDyn.f90
@@ -8,13 +8,18 @@ program MolDyn
   use iso_fortran_env, only : output_unit, error_unit
 
   !Declaration of global variables
-  character :: filename*100
-
+  character :: filename*100,class*3,atom*2
+  integer :: natom, steps
+  real :: mass, length, stept
 
   !Small user interface
   write(*,*) "Write te input data File name:"
   read(*,*) filename
+  call input_reader(filename,class,atom,mass,natom,length,steps,stept)
 
+  !Start variable check
+  write(*,*) class, atom, mass, natom, length, steps, stept
+  
 end program MolDyn
 
 
@@ -26,9 +31,19 @@ end program MolDyn
 
 
 !Subroutine for reading the input datas from the file
-subroutine reader(filename,class,atom,mass,natom,length,steps,stept)
+subroutine input_reader(filename,class,atom,mass,natom,length,steps,stept)
 
   !Declaration of local variables
+  character :: filename*100,class*3,atom*2
+  integer :: natom, steps, io
+  real :: mass, length, stept
 
+  !open input file
+  open(file=fileName,newunit=io)
+  
+  !Input of start Variables
+  read(io,*) class, atom, mass, natom, length, steps, stept
+  close(io)
 
-end subroutine reader
+end subroutine input_reader
+!---------------------------- --End of input reader subroutine------------------------------
